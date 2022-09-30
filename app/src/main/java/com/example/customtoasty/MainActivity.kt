1e945fab91e4a4c80618d0d194a4ec6bf607648f
ajay.pathania <ajay.pathania@vtnetzwelt.com>
2022-09-30 11:57:09 +0530

    Added maven-publish plugin

--- app/src/main/java/com/example/customtoasty/MainActivity.kt
+++ app/src/main/java/com/example/customtoasty/MainActivity.kt
@@ -7,6 +7,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        CustomToasty.showToaster(this,"Showing toast message by custom toasty library.")
+        CustomToasty.showToaster(this,"Showing toast message by custom toasty library.")
     }
 }
